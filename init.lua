9b663c485bae17d4bb9f83e1a953d059d279f104
Owen Dechow <owen.dechow@gmail.com>
2025-06-09 14:50:58 -0400

    Terminal sizing

--- init.lua
+++ init.lua
@@ -2,3 +2,15 @@ require("config.lazy")
 require("config.remap")
 require("config.opts")
 require("config.menu")
+
+vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
+  callback = function()
+    local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+    if not normal.bg then return end
+    io.write(string.format("\027]11;#%06x\027\\", normal.bg))
+  end,
+})
+
+vim.api.nvim_create_autocmd("UILeave", {
+  callback = function() io.write("\027]111\027\\") end,
+})
