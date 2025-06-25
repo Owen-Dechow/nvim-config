90d01b4cc785774ed849db0e873fb471ac139f3c
Owen Dechow <owen.dechow@gmail.com>
2025-06-25 15:38:28 -0400

    Specify mason tools

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,8 +1,33 @@
+-- MASON TOOLS TO BE INSTALLED
+local install = {
+    "djlint",
+    "findent",
+    "cbfmt",
+    "basedpyright",
+    "lua-language-server",
+    "fortls",
+    "json-lsp",
+    "markdown-oxide",
+    "oxlint",
+    "python-lsp-server",
+    "ruff",
+    "rust-analyzer",
+    "sourcery",
+}
+
 return {
     {
         "mason-org/mason.nvim",
         config = function()
             require("mason").setup({})
+            require("mason-registry").refresh(function()
+                for _, name in ipairs(install) do
+                    local p = require("mason-registry").get_package(name)
+                    if p and not p:is_installed() then
+                        p:install()
+                    end
+                end
+            end)
         end
     },
     {
