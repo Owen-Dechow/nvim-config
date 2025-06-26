92f9ddce1cf8aef9a53450b19080100d367dcdc1
Owen Dechow <owen.dechow@gmail.com>
2025-06-26 15:34:14 -0400

    color row

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -6,12 +6,12 @@ return {
         config = function()
             local background = "#0a0a0a"
             vim.cmd("colorscheme tokyodark")
-            vim.cmd("hi Normal guibg=" .. background)                        -- Darkk gray background
+            vim.cmd("hi Normal guibg=" .. background)                        -- Dark gray background
             vim.cmd("hi NonText guibg=" .. background)                       -- Ensures empty areas match
             vim.cmd("hi EndOfBuffer guibg=" .. background)                   -- Covers the end of buffer
             vim.cmd("hi NormalFloat guibg=" .. background)                   -- Covers the end of buffer
             vim.cmd("hi SignColumn guibg=" .. background)                    -- Dark gray background for sign column
-            vim.cmd("hi CursorLine guibg=" .. background)                    -- Dark gray background for cursor line:w
+            vim.cmd("hi CursorLine guibg=#152010")                           -- Dark gray background for cursor line
             vim.cmd("hi NormalNC guibg=" .. background)                      -- Dark gray background for inactive
 
             vim.cmd("hi DiagnosticUnderlineError gui=undercurl guisp=Red")   -- Squiggly red underline for errors
@@ -19,11 +19,11 @@ return {
             vim.cmd("hi DiagnosticUnderlineInfo gui=undercurl guisp=Blue")   -- Squiggly blue underline for info
             vim.cmd("hi DiagnosticUnderlineHint gui=undercurl guisp=Green")  -- Squiggly green underline for hints
 
-            vim.cmd("hi CursorLineNr guifg=#508020 gui=bold") -- Cursor line color
-            vim.cmd("set cursorline") -- Enable Cursor line
+            vim.cmd("hi CursorLineNr guifg=#608040 gui=bold")                -- Cursor line color
+            vim.cmd("set cursorline")                                        -- Enable Cursor line
 
-            vim.o.colorcolumn = "72,80,90" -- Highlights columns 72, 80, and 100
-            vim.cmd("hi ColorColumn guibg=#0f0f0f") -- Dark gray background for color column
+            vim.o.colorcolumn = "80"                                         -- Highlights columns 72, 80, and 100
+            vim.cmd("hi ColorColumn guibg=#0f0f0f")                          -- Dark gray background for color column
         end
     },
 }
--- lua/plugins/spell.lua
+++ lua/plugins/spell.lua
@@ -1,36 +1,81 @@
 return {
-    -- dir = "C:\\Users\\owend\\Desktop\\nvim_wurd_spell",
     "Owen-Dechow/nvim_wurd_spel",
     opts = {
         -- severity = vim.diagnostic.severity.INFO,
-        -- -- Set the severity level of spelling errors
-        -- -- INFO, WARN, ERROR
+        -- -- Set the severity level of spelling errors:
+        --     INFO, WARN, or ERROR.
 
         -- min_length = 5,
-        -- -- Set the minimum length of word to be checked
+        -- -- Set the minimum length of word to be checked.
 
         -- ignore = { "Dechow", "Neovim" }
-        -- -- Add words to be ignored
+        -- -- Add words to be ignored.
         -- -- IMPORTENT: words added to Neovim's built in
-        --    spelling ignore dictionary are still ignored.`
-        --    It is suggested to add words to that dictionary
-        --    using `zg`, `:spellgood`, or from the WurdSpelSuggest
-        --    options instead of adding words here.
+        --     spelling ignore dictionary are still ignored.`
+        --     It is suggested to add words to that dictionary
+        --     using `zg`, `:spellgood`, or from the
+        --     `:WurdSpel suggest` options instead of adding
+        --     words here.
 
         -- enabled = true
-        -- -- Enable WurdSpel on start
+        -- -- Enable WurdSpel on start.
 
         -- remap = true
         -- -- Remap the builtin z=, zg, & zw commands to the
-        -- -- WurdSpel command equivalents.
-        -- -- z= -> WurdSpelSuggest
-        -- -- zg -> WurdSpelGood
-        -- -- zw -> WurdSpelBad
+        --     WurdSpel command equivalents:
+        --     z= -> WurdSpel suggest
+        --     zg -> WurdSpel good
+        --     zw -> WurdSpel bad
 
         -- remap_special = false
-        -- -- Add special remaps
-        -- -- <leader>zz -> WurdSpelSuggest
-        -- -- <leader>zg -> WurdSpelGood
-        -- -- <leader>zw -> WurdSpelBad
+        -- -- Add special remaps:
+        --     <leader>zz -> WurdSpel suggest
+        --     <leader>zg -> WurdSpel good
+        --     <leader>zw -> WurdSpel bad
+
+        -- buf_option_guards = {
+        --    modifiable = true,
+        --    buftype = "",
+        -- }
+        -- -- These checks determine if the spellchecker will be
+        --     attached to the buffer. The value will be gotten
+        --     using `nvim_buf_get_option(0, ...)`.
+        -- -- If the result of
+        --     `nvim_buf_get_option` is not equal to the specified
+        --     value in the table then the spellchecker will not
+        --     attach.
+
+        -- pattern = { "*" },
+        -- -- Pattern for auto commands.
+
+        -- allow_one_letter_prefix = true
+        -- -- Allow one letter prefix on words:
+        --     `ilocal` -> correct because local is correct.
+        -- -- If this setting is false:
+        --     `ilocal` -> incorrect because ilocal is not a word.
+
+        -- num_options = 5,
+        -- -- Set the maximum number of options in the spell
+        --     suggest menu.
+
+        -- add_to_settings_in_suggest = true,
+        -- -- Add the `[Add to user settings]` option in the
+        --     spell suggest menu.
+
+        -- add_to_settings_at_end = true,
+        -- -- Control the location of the `[Add to user settings]`
+        --     option in the spell suggest menu.
+        -- -- If this setting is true `[Add to user settings]` will
+        --     be at the end.
+        -- -- If this setting is false `[Add to user settings]` will
+        --     be at the start.
+
+        -- message_prefix = "(zz) "
+        -- -- Adds a prefix to the "Possible misspelling ..." error
+        --     message.
+        -- -- You may use any string value for this option.
+        -- -- Examples:
+        --     "(zz) " ->  "(zz) Possible misspelling..."
+        --     ""      ->  "Possible misspelling..."
     }
 }
