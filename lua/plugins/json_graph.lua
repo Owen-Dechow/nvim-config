a6b107388ed132a52a413d15b21dc7f776c9098f
Owen Dechow <owen.dechow@gmail.com>
2025-07-18 21:11:46 -0400

    JsonGraphView -> Videre

--- lua/plugins/json_graph.lua
+++ lua/plugins/json_graph.lua
@@ -1,6 +1,10 @@
 return {
     "Owen-Dechow/nvim_json_graph_view",
-    -- dir = "~\\Desktop\\nvim_json_graph_view",
+    dir = "~\\Desktop\\nvim_json_graph_view",
+    dependencies = {
+        "Owen-Dechow/graph_view_yaml_parser_test",
+        dir = "~\\Desktop\\graph_view_yaml_parser_test"
+    },
     opts = {
         round_units = false,
     }
--- lua/plugins/lsp_sig.lua
+++ lua/plugins/lsp_sig.lua
@@ -2,13 +2,12 @@ return {
     "ray-x/lsp_signature.nvim",
     event = "InsertEnter",
     config = function()
-        vim.api.nvim_set_hl(0, "LspSignatureVirtualText", { fg = "#44ee44" })
         require("lsp_signature").setup({
             hint_prefix = "",
             hint_inline = function() return "eol" end,
             doc_lines = 0,
             floating_window = false,
-            hint_scheme = "LspSignatureVirtualText"
+            hint_scheme = "Comment"
         })
     end
 }
