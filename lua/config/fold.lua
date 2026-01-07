21a3e6eaaf34a0d6a8148337ea67aebc8e3da1e2
Owen Dechow <owen.dechow@gmail.com>
2026-01-07 17:21:58 -0500

    update

--- lua/config/fold.lua
+++
@@ -1,6 +0,0 @@
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.opt.foldenable = true
-vim.opt.foldlevel = 0
-vim.opt.foldlevelstart = 99
-
--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -28,8 +28,15 @@ vim.o.undodir = vim.env.HOME .. "/.vim/undodir"
 vim.o.undofile = true
 
 vim.diagnostic.enable()
-vim.diagnostic.config({ signs = true, virtual_text = true })
+vim.diagnostic.config({ signs = false, virtual_text = true })
 
 vim.wo.wrap = false
 
 vim.o.winborder = 'rounded'
+
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldenable = true
+vim.opt.foldlevel = 0
+vim.opt.foldlevelstart = 99
+vim.opt.foldcolumn = "0"
