aafe0cce133913ae89158c4e89ae4d1fb93df4f4
Owen Dechow <owen.dechow@gmail.com>
2026-01-30 10:19:42 -0500

    dsh lang

--- lua/config/custom_langs.lua
+++ lua/config/custom_langs.lua
@@ -20,18 +20,34 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     end,
 })
 
-local sigil_group = vim.api.nvim_create_augroup("SigilHighlightGroup", { clear = true })
+local sigil_group = vim.api.nvim_create_augroup("DSharpHighlightGroup", { clear = true })
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     group = sigil_group,
-    pattern = "*.sigil",
+    pattern = "*.dsh",
     callback = function()
-        vim.cmd [[set filetype=SigilLang]]
-        vim.cmd [[syn keyword Keyword invoke sigil src and or]]
-        vim.cmd [[syn match Comment "#.*$"]]
+        vim.cmd [[set filetype=DSharp]]
+
+        vim.cmd [[syn match Identifier "\v[A-Za-z_]\w*"]]
+
+        vim.cmd([[syn keyword Keyword public ]]
+            .. [[private protected class open ]]
+            .. [[void throws throw return break ]]
+            .. [[continue static for while loop ]]
+            .. [[enum match from using in ]]
+            .. [[extends let as if else capture]])
+
+        vim.cmd [[syn match Comment "//.*$"]]
         vim.cmd [[syn region String	start=+"+ skip=+\\\\\|\\"+ end=+"+]]
         vim.cmd [[syn match Number "\v<[0-9]%(_*[0-9])*>|<0[xX]_*[0-9a-fA-F]%(_*[0-9a-fA-F])*>|<0[oO]_*%(_*[0-7])*>|<0[bB]_*[01]%(_*[01])*>"]]
-        vim.cmd [[syn match Operator "[<>=*/+-.:?%$!]"]]
-        vim.cmd [[syn keyword Structure Whisper Loop]]
+        vim.cmd [[syn match Operator "[<>=*+-.:?%$!]"]]
+        vim.cmd [[syn keyword Type i32 f32 bool u32 u8 string ExitError ValueError]]
+        vim.cmd [[syn keyword Boolean true false]]
+
+        vim.cmd [[syn match Type "\v(class\s+)@<=([A-Za-z_]\w*)"]]
+        vim.cmd [[syn match Type "\v(enum\s+)@<=([A-Za-z_]\w*)"]]
+
+        vim.cmd [[syn match Function "\v([A-Za-z_]\w*)(\(.*\))@="]]
+        vim.cmd [[syn match Function "\v([A-Za-z_]\w*)(\s*\.\s*[A-Za-z_])@="]]
     end
 })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -29,7 +29,9 @@ end
 return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+    },
     config = function()
         require("telescope").setup({
             defaults = {
