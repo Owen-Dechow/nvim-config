8159b9b5721bf649085812c9f79773b3c56cc893
Owen Dechow <owen.dechow@gmail.com>
2026-01-29 20:34:00 -0500

    Set treesitter master

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
+        branch = "master",
         config = function()
             require("nvim-treesitter.configs").setup({
                 auto_install = true,
