b324cb930266f59e4ea770448ddf3c9cf8ac605a
Owen Dechow <owen.dechow@gmail.com>
2026-03-19 20:43:02 -0400

    3/19/26

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,15 +21,10 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
--- Setup lazy.nvim
 require("lazy").setup({
     spec = {
-        -- import your plugins
         { import = "plugins" },
     },
-    -- Configure any other settings here. See the documentation for more details.
-    -- colorscheme that will be used when installing plugins.
     install = { colorscheme = { "habamax" } },
-    -- automatically check for plugin updates
-    checker = { enabled = true },
+    checker = { enabled = false },
 })
